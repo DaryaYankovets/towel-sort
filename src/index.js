4a6c88ea4984537723b632f594bcf275a525661b
YankovetsDarya <yankovets.daria@gmail.com>
2022-01-11 10:56:44 +0300

    feat: add solution function towelSort (matrix)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 
 // You should implement your task here.
 
+const { underline } = require("colors");
+
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr = [];
+  let count = 1;
+  if (!arguments.length) {
+    return [];
+  }
+  matrix.forEach(elem => {
+    count % 2 !== 0 ? arr.push(...elem) : arr.push(...elem.reverse());
+    count++;
+  });
+  return arr;
 }
